ef46a96727bda2b15450a3a5b71ef7479f54b9d3
Adérito Marrime <AderitoMarrime@users.noreply.github.com>
2024-08-21 09:37:34 +0200

    cadastrar em php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+    require("conexao.php");
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,8 +15,8 @@
         <div class="primeira">
             <label for="usuario">Usuario</label>
             <input type="text" name="usuario" id="usuario">
-            <label for="usuario">Usuario</label>
-            <input type="text" name="usuario" id="usuario">
+            <label for="senha">Senha</label>
+            <input type="password" name="senha" id="senha">
         </div>
         <div class="segunda">
             <input type="submit" value="Cadastrar" name="cadastrar">
@@ -20,3 +24,29 @@
     </form>
 </body>
 </html>
+
+<?php
+    if (isset ($_POST['cadastrar'])) {
+        $usuario = filter_input(INPUT_POST, "usuario", FILTER_SANITIZE_SPECIAL_CHARS);
+        $senha = filter_input(INPUT_POST, "senha", FILTER_SANITIZE_SPECIAL_CHARS);
+        
+        if (!empty($usuario) && !empty($senha)) {
+
+            $sql = "INSERT INTO usuarios (nome_de_usuario, senha) VALUES ('$usuario','$senha')";
+
+            try {
+                mysqli_query($conexao, $sql);
+                echo"<script> alert('usuario cadastrado com sucesso')</script>";
+            }
+
+            catch(mysqli_sql_exception) {
+                echo"<script> alert('O nome de usuario {$usuario} esta indisponivel, tente outro')</script>";
+            }
+
+        } else {
+            echo"<script> alert('preencha todos os campos')</script>";
+        }
+    }
+
+    mysqli_close($conexao);
+?>
